99dcbf55487e9904111f813cca0a945a17dcdcb7
pro-utkarshM <118096976+pro-utkarshM@users.noreply.github.com>
2025-11-23 16:29:23 -0500

    feat: Add RGB image support and data structures
    - Add rgb_image_t struct with separate R, G, B channels
    - Implement load_image_as_rgb() for color image loading
    - Implement make_resized_rgb() with channel-wise averaging
    - Add free_rgb_image() for proper memory cleanup
    - Add rgb_to_grayscale() with standard luminance weights
    - Prepare foundation for colorized ASCII output

--- include/image_processing.h
+++ include/image_processing.h
@@ -9,6 +9,14 @@ typedef struct {
     unsigned char* data;
 } grayscale_image_t;
 
+typedef struct {
+    size_t width;
+    size_t height;
+    unsigned char* r_data;
+    unsigned char* g_data;
+    unsigned char* b_data;
+} rgb_image_t;
+
 grayscale_image_t load_image_as_grayscale(const char* file_path);
 
 grayscale_image_t make_resized_grayscale(grayscale_image_t* original, size_t max_width, size_t max_height);
@@ -16,4 +24,12 @@ grayscale_image_t make_resized_grayscale(grayscale_image_t* original, size_t max
 void print_image(grayscale_image_t* image, bool dark_mode);
 
 void free_grayscale_image(grayscale_image_t* image);
+
+rgb_image_t load_image_as_rgb(const char* file_path);
+
+rgb_image_t make_resized_rgb(rgb_image_t* original, size_t max_width, size_t max_height);
+
+void free_rgb_image(rgb_image_t* image);
+
+grayscale_image_t rgb_to_grayscale(rgb_image_t* rgb);
 #endif
